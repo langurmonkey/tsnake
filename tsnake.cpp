ab75138af9d8372fa2ebca06280ae07328c25cf6
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-03-06 21:28:12 +0100

    Fix self-collision

--- tsnake.cpp
+++ tsnake.cpp
@@ -31,7 +31,6 @@ void paint_snake();
 void do_chdir(int newy, int newx, int newdir, int opposite_dir);
 int out_of_boudns(int y, int x);
 int is_move_hit(int y, int x);
-int is_move_food(int y, int x);
 void draw_map(void);
 void print_bottom(char* text);
 void create_food();
@@ -60,12 +59,23 @@ int main(void)
 
     /* initialize curses */
     initscr();
+    if(has_colors() == FALSE) {
+        endwin();
+        printf("Your terminal does not support color\n");
+        exit(1);
+    } else {
+        start_color();
+    }
     keypad(stdscr, TRUE);
     cbreak();
     noecho();
     nodelay(stdscr, TRUE);
     curs_set(0);
-
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLUE);
+    init_pair(3, COLOR_RED, COLOR_GREEN);
+    init_pair(4, COLOR_WHITE, COLOR_BLACK);
+    init_pair(5, COLOR_BLUE, COLOR_BLACK);
     clear();
 
     if(LINES < 10 || COLS < 35){
@@ -90,6 +100,7 @@ int main(void)
     start = clock();
 
     /* init snake */
+    wbkgd(gamew, COLOR_PAIR(3));
     for(int i = 0; i < START_LEN; i++) {
         point p = {x + 1 - START_LEN + i, y};
         snake.push_front(p);
@@ -203,10 +214,13 @@ int main(void)
             create_food();
             nfood = 1;
             score++;
+            if (score % 20 == 0)
+                speed = std::clamp(speed / 2.0F, 0.03125F, 1.0F);
         }
 
         
         /* refresh */
+        wbkgd(gamew, COLOR_PAIR(5));
         box(gamew, 0, 0);
         wrefresh(gamew);
         refresh();
@@ -214,7 +228,7 @@ int main(void)
     while (running);
 
     /* done */
-    std::string msg = "Press any key to quit";
+    std::string msg = "Quit (q) or restart (r)?";
     int minl = msg.size() + 2;
     int ew_w = std::clamp(COLS / 2, minl, COLS);
     int ew_h = std::clamp(LINES / 2, 4, LINES);
@@ -233,9 +247,12 @@ int main(void)
 
 void print_status(const char* status)
 {
+    attron(COLOR_PAIR(1));
     mvhline(LINES - 1, 0, EMPTY, COLS);
     move(LINES - 1, 0);
     printw(status);
+    attroff(COLOR_PAIR(1));
+    refresh();
 }
 
 point rd()
@@ -261,11 +278,14 @@ void create_food()
     point newp = rd();
     food.x = newp.x;
     food.y = newp.y;
+    wbkgd(gamew, COLOR_PAIR(2));
     mvwaddch(gamew, food.y, food.x, FOOD);
+    
 }
 
 void update(int x, int y)
 {
+    wbkgd(gamew, COLOR_PAIR(3));
     point newpoint = {x, y};
     snake.push_front(newpoint);
     if(!nfood){
@@ -314,14 +334,15 @@ int out_of_bounds(int y, int x)
 
 int is_move_hit(int y, int x)
 {
-    int testch = mvinch(y, x);
-    return testch == SNAKE || out_of_bounds(y, x);
-}
-
-int is_move_food(int y, int x)
-{
-    int testch = mvinch(y, x);
-    return testch == FOOD;
+    int selfhit = 0;
+    std::deque<point>::iterator it = snake.begin();
+    while (it != snake.end()){
+        selfhit = it->x == x && it->y == y;
+        if(selfhit)
+            break;
+        *it++;
+    }
+    return selfhit || out_of_bounds(y, x);
 }
 
 void draw_map(void)
